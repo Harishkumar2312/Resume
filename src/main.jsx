279c86b36d7cfa7056fe2c2d2370034c7c09f4b5
Harishkumar R <mail2haris2002@gmail.com>
2025-04-05 21:43:22 +0530

    comit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 // import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import Content1 from './Content1'
+import Content1 from './Component/First/Content1'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
