7d6ecc2c8f5a04f9114fad1aa7313c60225b39b2
Harishkumar R <mail2haris2002@gmail.com>
2025-04-08 10:27:18 +0530

    com

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 // import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import Content1 from './Component/First/Content1'
+// import Content1 from './Component/First/Content1'
+import Sports from './Component/Sports'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Content1/>
+    {/* <Content1/> */}
+    <Sports/>
   </StrictMode>,
 )
